b7ad2b6157e112c6dce7f18f69ff0706699faa96
Dhruv Bhagatkar <bhagatkardhruv2003@gmail.com>
2026-07-29 23:03:34 +0530

    Update App.tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -25,7 +25,7 @@ export default function App() {
   // Mobile: shake to activate
   useEffect(() => {
     let lastShake = 0;
-    const SHAKE_THRESHOLD = 60;
+    const SHAKE_THRESHOLD = 90;
     const COOLDOWN = 3000;
 
     const handleMotion = (e: DeviceMotionEvent) => {
